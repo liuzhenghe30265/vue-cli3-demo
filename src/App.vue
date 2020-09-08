c49607e3a6531182156667aa90b8ec5619a685ae
liuzhenghe30265 <15901450207@163.com>
2020-09-08 18:23:51 +0800

    feat:自定义指令弹窗拖拽

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,71 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div v-drag>
+      <div class="drag-body">
+        <div class="drag-header">header
+        </div>
+        <div class="drag-content">
+          content
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import Vue from 'vue'
+Vue.directive('drag', {
+  // eslint-disable-next-line no-unused-vars
+  bind(el, binding, vnode) {
+    const dragBody = el.querySelector('.drag-body')
+    const dragHeader = el.querySelector('.drag-header')
+    dragHeader.style['cursor'] = 'move'
+    // window.getComputedStyle 获取元素当前样式，只有 IE 和 Opera 支持 currentStyle
+    const currentStyle = dragBody.currentStyle || window.getComputedStyle(dragBody, null)
+    dragHeader.onmousedown = (e) => {
+      // 计算按下鼠标的位置据窗口可视区域的距离
+      const distanceX = e.clientX - dragHeader.offsetLeft
+      const distanceY = e.clientY - dragHeader.offsetTop
+      // 将 px 为单位的字符串改为数值
+      const styleLeft = +currentStyle.left.replace(/\px/g, '')
+      const styleTop = +currentStyle.top.replace(/\px/g, '')
+      document.onmousemove = (e) => {
+        // 移动的距离
+        const moveLeft = e.clientX - distanceX
+        const moveTop = e.clientY - distanceY
+        // 移动当前元素
+        dragBody.style.left = `${moveLeft + styleLeft}px`
+        dragBody.style.top = `${moveTop + styleTop}px`
+      }
+      document.onmouseup = () => {
+        document.onmousemove = null
+        document.onmouseup = null
+      }
     }
+  }
+})
+export default {
+  name: 'App',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.drag-body {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 400px;
+  height: 600px;
+  border: 1px solid;
+  .drag-header {
+    padding: 20px;
+    border: 1px solid;
+  }
+  .drag-content {
+    padding: 20px;
   }
 }
 </style>
